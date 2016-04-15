9b8c6992cf3c89707f1e07958829e1573605fbcb
mechamogera <mechamosura@gmail.com>
2016-04-15 12:21:20 +0900

    Support CloudWatch Logs Metric Filters

--- test/plugin/test_in_cloudwatch.rb
+++ test/plugin/test_in_cloudwatch.rb
@@ -116,4 +116,30 @@ class CloudwatchInputTest < Test::Unit::TestCase
     assert_equal true, d.instance.delayed_start
   end
 
+  ### for CloudWatchLogsMetricFilters
+  CONFIG_CWLOG_MF = %[
+    tag cloudwatch
+    aws_key_id test_key_id
+    aws_sec_key test_sec_key
+    cw_endpoint test_cloud_watch_endpoint
+    namespace LogMetrics
+    metric_name LogAccessCount,LogErrorCount
+  ]
+
+  def create_driver_cwlog_mf(conf = CONFIG_CWLOG_MF)
+     Fluent::Test::InputTestDriver.new(Fluent::CloudwatchInput).configure(conf)
+  end
+
+  def test_configure_cwlog_mf
+    d = create_driver_cwlog_mf
+    assert_equal 'cloudwatch', d.instance.tag
+    assert_equal 'test_key_id', d.instance.aws_key_id
+    assert_equal 'test_sec_key', d.instance.aws_sec_key
+    assert_equal 'test_cloud_watch_endpoint', d.instance.cw_endpoint
+    assert_equal 'LogMetrics', d.instance.namespace
+    assert_equal 'LogAccessCount,LogErrorCount', d.instance.metric_name
+    assert_nil d.instance.dimensions_name
+    assert_nil d.instance.dimensions_value
+    assert_equal [], d.instance.dimensions
+  end
 end
